312c03ba7e092fb87a7e6e6be78c9eef61982096
ShahzaibMirza-10 <150156134+ShahzaibMirza-10@users.noreply.github.com>
2023-11-21 00:32:48 +0500

    added

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="loading">
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+
+        <div className="cooking">
+            <div className="pan">
+                <span className="inner"></span>
+                <span className="handle"></span>
+            </div>
+            <div className="food">
+                <div className="cake"></div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default App
